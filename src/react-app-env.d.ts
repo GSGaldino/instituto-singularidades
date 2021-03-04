7291688c1981e4bafedb2abbbde8edfbb7b92848
Gabriel Soares <gabrielsoaresii@outlook.com>
2021-03-04 13:30:43 -0300

    fix: prudiction changes

--- src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react-scripts" />
-declare module '*.ogg' {
-    const src: string;
-    export default src;
-}
-declare module "*.json" {
-    const value: any;
-    export default value;
-}
